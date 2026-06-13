fa266421bc90bd5b43355da62c54550195c9341d
Gianni B <gianni.bonicolini@outlook.com>
2026-06-13 19:54:04 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MinimalApi Project Template
+
+Custom template for creating a minimal API project with .NET 10 and SwaggerUI already configured.
---
+++ src/MinimalApiProjectTemplate/Program.cs
@@ -0,0 +1,44 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+app.MapOpenApi();
+
+app.UseSwaggerUI(options =>
+{
+	options.SwaggerEndpoint("/openapi/v1.json", app.Environment.ApplicationName);
+});
+
+app.UseHttpsRedirection();
+#if WithWeatherForecast
+
+var summaries = new[]
+{
+	"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+	var forecast = Enumerable.Range(1, 5).Select(index =>
+		new WeatherForecast
+		(
+			DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+			Random.Shared.Next(-20, 55),
+			summaries[Random.Shared.Next(summaries.Length)]
+		))
+		.ToArray();
+	return forecast;
+})
+.WithName("GetWeatherForecast");
+#endif
+
+app.Run();
+#if WithWeatherForecast
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+#endif
